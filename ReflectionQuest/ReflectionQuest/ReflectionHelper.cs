21bc44b0f606b66bb3dd4dde58307b18d1a20887
nicocababie <cababienico@gmail.com>
2023-10-06 10:00:35 -0300

    Ejercicio 2 y 3

--- ReflectionQuest/ReflectionQuest/ReflectionHelper.cs
+++ ReflectionQuest/ReflectionQuest/ReflectionHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,7 +22,26 @@ namespace ReflectionQuest
         {
             Console.WriteLine("Get the name of a method with local variables of type bool and int");
             Assembly assembly = Assembly.LoadFrom(assemblyPath);
-            Console.ReadLine();
+
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                MethodInfo[] methods = type.GetMethods();
+                foreach (MethodInfo method in methods)
+                {
+                    if (method.GetMethodBody() != null)
+                    {
+                        LocalVariableInfo[] localVariables = method.GetMethodBody().LocalVariables.ToArray();
+                        var localVariablesNames = localVariables.Select(x => x.LocalType.Name.Equals("bool") && 
+                        x.LocalType.Name.Equals("int")).ToArray();
+                        if (localVariablesNames.Length > 0)
+                        {
+                            Console.WriteLine(method.Name);
+                        }
+                    }
+                }
+            }   
             throw new NotImplementedException("Not implemented D:");
         }
 
@@ -29,7 +49,16 @@ namespace ReflectionQuest
         {
             Console.WriteLine("Get the name of a type that implements IEnumerable");
             Assembly assembly = Assembly.LoadFrom(assemblyPath);
-            Console.ReadLine();
+
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                if (type.IsAssignableFrom(typeof(IEnumerable<>)))
+                {
+                    Console.WriteLine(type.Name);
+                }
+            }
             throw new NotImplementedException("Not implemented D:");
         }
 
